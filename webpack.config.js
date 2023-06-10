1fd33945d12bbd57280088e0ca2466dfacf279c1
ggreer91 <georgegreer91@gmail.com>
2023-06-09 20:44:13 -0700

    Adds index.html, styles.css, and configures webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  module: {
+      rules: [
+        {
+          test:  /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }], ['@babel/preset-react', { targets: "defaults" }],
+              ]
+            }
+          }
+        },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          "css-loader",
+        ],
+      },
+      ]
+    },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './client/index.html',
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+      publicPath: '/',
+    },
+    compress: true,
+    port: 8080
+  }
+}
