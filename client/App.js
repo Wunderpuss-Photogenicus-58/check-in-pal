121401d85e314e6934e650d09cc1ff7ea70d413c
ggreer91 <georgegreer91@gmail.com>
2023-06-10 11:12:31 -0700

    Adds CheckinButton to Home, Home to App, and minor styling on Home
    Co-authored by: John Kim

---
+++ client/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from './Containers/Home.js'
+
+const App = () => {
+
+
+
+
+
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from './App.js';
+import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <div className="main">
   <App />
   </div>
-)
+);
