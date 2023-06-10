58cd5d8c217e4a1ad11d6944536081900be7c6e5
ggreer91 <georgegreer91@gmail.com>
2023-06-10 10:39:46 -0700

    Adds functionality in index.js, updates index.html

--- client/index.js
+++ client/index.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from './App.js';
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <div className="main">
+  <App />
+  </div>
+)
