a7c81d4d5b2c5ea4dafa3abcd23eea09aa6fe34b
Daniel Park <danielpark@Daniels-MacBook-Air.local>
2023-06-11 12:42:52 -0700

    beings working on checkout controller and checkout route
    Co-authored-by: Daniel https://github.com/parkdaniel731
    Co-authored-by: Cyrux https://github.com/cyduckk

--- server/controller/checkoutController.js
+++ server/controller/checkoutController.js
@@ -1,3 +1,61 @@
+const db = require('../models/activityModel');
+
+
 const checkoutController = {};
+
+checkoutController.postTotals = async (req, res, next) => {
+  const activity = 'codewars';
+  const starttime = 1100;
+  const day = '06/10';
+ 
+  try {
+    const text = `
+    INSERT INTO time_card (activity, starttime, day)
+    VALUES ($1, $2, $3)
+    `;
+
+    const params = [activity, starttime, day];
+    const result = await db.query(text, params);
+    console.log('result is: ', result);
+    next();
+  }
+  
+  catch(err) {
+    next({
+      log: 'Express error handler caught in checkoutController.postTotals middleware',
+      status: 400,
+      message: { err: 'Failed to insert totals to database' }
+    });
+  }
+
+};
+
+checkoutController.getTimecard = async (req, res, next) => {
+  const activity = 'codewars';
+  const starttime = 1100;
+  const day = '06/10';
+ 
+  try {
+    const text = `
+    INSERT INTO time_card (activity, starttime, day)
+    VALUES ($1, $2, $3)
+    `;
+
+    const params = [activity, starttime, day];
+    const result = await db.query(text, params);
+    console.log('result is: ', result);
+    next();
+  }
+  
+  catch(err) {
+    next({
+      log: 'Express error handler caught in checkoutController.getTimecard middleware',
+      status: 400,
+      message: { err: 'Failed to get timecard from database' }
+    });
+  }
+
+};
+
 
 module.exports = checkoutController;
--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
-const activityController = require('../controller/checkoutController');
+const checkoutController = require('../controller/checkoutController');
 
 const router = express.Router();
+
+router.get('/', checkoutController.postTotals, checkoutController.getTimecard, (req, res) => {
+  res.status(200).json(res.locals.timecard);
+});
 
 module.exports = router;
