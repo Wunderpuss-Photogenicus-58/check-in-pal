3a28eaceb58f502ce0fe09391cad0527e727f4fe
ggreer91 <georgegreer91@gmail.com>
2023-06-12 14:02:16 -0700

    Adds CheckoutDisplay component and associated Route to App.jsx
    Co-authored-by: Daniel https://github.com/parkdaniel731
    Co-authored-by: Cyrux https://github.com/cyduckk
    Co-authored-by: George https://github.com/ggreer91
    Co-authored-by: John https://github.com/jayoo0621

--- client/App.jsx
+++ client/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Home from './Containers/Home.jsx';
 import SwitchDisplay from './Containers/SwitchDisplay.jsx';
 import ActivityDisplay from './Containers/ActivityDisplay.jsx';
+import CheckoutDisplay from './Containers/CheckoutDisplay.jsx';
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+
 const App = () => {
   // create state called 'activity' and assign it to empty string and it's setter function as 'setActivity'
   const [ activity, setActivity ] = useState('');
   // console.log(activity);
 
-
-  
   // return the routes , each rendering a component at their respective paths
   return (
      // create routes
@@ -18,6 +18,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/switch" element={<SwitchDisplay activity={activity} setActivity={setActivity} />} />
       <Route path="/activity" element={<ActivityDisplay activity={activity} />} />
+      <Route path="/checkout" element={<CheckoutDisplay />} />
     </Routes>
   );
 }
